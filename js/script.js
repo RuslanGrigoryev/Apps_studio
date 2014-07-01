e1cabf4d1042593634897ff2b73be9a9c9aa2d0d
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-02 00:37:32 +0300

    Вернул назад старый слайдер, файлы не удалял для отката

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@ $(function() {
 	
     $('.anim').show().addClass('fadeIn animated');
 
-    if ( $("#slider").length) {
-        $("#slider").skitter({ animation: "cubeSize", hideTools: true, navigation: false, numbers: false });
-    }
+   
+
+        /*Main slider*/
+    $("#slider").find(".item:gt(0)").hide();
+
+    setInterval(function() { 
+      $('#slider').find(".item:first")
+        .fadeOut(2000)
+        .next()
+        .fadeIn(2000)
+        .end()
+        .appendTo('#slider');
+    },  6000);
 
 
     /*Slide punkts on page how_it_works_2*/
